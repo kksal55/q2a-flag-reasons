440e0d950886b85fbbe9406d0015ccf271896caa
kksal55 <kksal55@users.noreply.github.com>
2019-05-24 15:12:29 +0300

    Add files via upload

---
+++ q2apro-flag-reasons-lang-default.php
@@ -0,0 +1,21 @@
+<?php
+/*
+	Plugin Name: q2apro Flag Reasons
+*/
+
+return array(
+	'reason' => 'Uyarı Sebebiniz:',
+	'reason_spam' => 'Başlık Hatalı',
+	'reason_quality' => 'Müstehcen İçerik',
+	'reason_rude' => 'Tekrar Sorulmuş Soru',
+	'reason_edit' => 'Konu Dışı',
+	'reason_duplicate' => 'Tartışma Konusu',
+	'reason_offtopic' => 'Off-topic',
+	'reason_migrate' => 'Migrate',
+	'reason_close' => 'Close',
+	'reason_other' => 'Other',
+	'note' => 'Ek Not:',
+	'enter_details' => 'Detaylar …',
+	'send' => 'Kaydet',
+	'flaglist_head' => 'Uyarı:',
+);
